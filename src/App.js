2e3c5f39a72e8925b333c31b357c429ebc2696b8
Dino <agboalez@gmail.com>
2024-11-20 11:23:24 +0300

    20 november

--- src/App.js
+++ src/App.js
@@ -25,14 +25,19 @@ function App() {
     )
   };
 
+  const desig = (hooks.formatDate.time).slice(0,2) < Number(12) ? "AM" 
+                : "PM"
+
 
   return (
     <div className="Appx" 
      style={{display:"grid", placeContent:"center", width:"auto", minHeight:"100vh", placeItems:"initial", backgroundColor:"rgba(50, 50, 200, 0.5)"}}
       >
-      <h1>{hooks.formatDate.weekDay}</h1>
-     <div>{hooks.formatDate.time}</div>
-     <div>{hooks.formatDate.fullDate()}</div>
+        <div style={{display:"flex", justifyContent:"space-around", paddingBottom:"10px"}}>
+         <strong>{hooks.formatDate.weekDay}</strong>
+         <div>{hooks.formatDate.time} {desig}</div>
+         <div>{hooks.formatDate.date} {hooks.formatDate.month}</div>
+        </div>
      <div className='btn-cont' style={{position:"fixed", top:"0", left:"0", right:"0", backgroundColor:"white", padding:"6px"}}>
       {user && <button className='btn' onClick={()=> navigate("/allData")}>View</button>}
       {user && <button className='btn' onClick={()=> {hooks.clearLocalStorage("user") ; navigate("/auth"); setUser("")}}>Logout</button>}
