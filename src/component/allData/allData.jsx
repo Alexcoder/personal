234b4011a1167e0e3e2529cf3f40745b85923de6
Dino <agboalez@gmail.com>
2024-11-25 15:52:49 +0300

    25 november

--- src/component/allData/allData.jsx
+++ src/component/allData/allData.jsx
@@ -5,6 +5,10 @@ import "./allData.css"
 
 const AllData = () => {
     const [datafromDB, setDatafromDB] = useState([])
+    const [budgetItem, setBudgetItem] = useState("");
+    const [budgetId, setBudgetId] = useState("");
+    const [postItem, setPostItem] = useState("");
+    const [fullPost, setFullPost] = useState(false);
     const {user, reqId, setReqId, loading, setLoading} = useGlobalState();
     console.log("datafromDB", datafromDB)
 
@@ -112,6 +116,22 @@ function budgetColor(status){
           : "green"
 };
 
+const view=(postItem, budgetItem, budgetId)=>{
+    setFullPost(prev=> !prev)
+    setBudgetItem(budgetItem)
+    setPostItem(postItem)
+    setBudgetId(budgetId)
+}
+
+const check=(budgetDetail)=>{
+    if(budgetDetail.length>13){
+        return "..."
+    }else{
+        return
+    }
+
+}
+
   return (
     <div className='allData'>
         {loading ? "page loading...": datafromDB.length<1? "No Data Found": ""}
@@ -129,29 +149,69 @@ function budgetColor(status){
                 <div><strong className="item">{sum(item.month, item.year)}</strong></div>
                 <section className='budgetWrapper'>
                 {item?.budget.reverse().map(budget=>(
-                    <div key={budget?._id} className="budget" 
+                    <div 
+                    key={budget?._id} 
+                    className="budget" 
+                    onClick={()=> view(item, budget.expenseList[0], budget?._id)}
                     style={{backgroundColor: budgetColor(budget.expenseList[0].status)}}
                     >
                     {/* <section style={{display:"flex", justifyContent:"space-between"}}> */}
 
                         <div className="budgetItem" style={{fontSize:"14px", textTransform:"uppercase", color:"black", fontWeight:"650"}}>{budget.expenseList[0].purpose}</div>
-                        <div className="budgetItem" style={{textAlign:"start",fontSize:"14px", fontStyle:"italic"}}>{budget.expenseList[0].detail.slice()}</div>
+                        <div className="budgetItem" style={{textAlign:"start",fontSize:"14px", fontStyle:"italic"}}>{budget.expenseList[0].detail.slice(0,13)}{check(budget.expenseList[0].detail)}</div>
 
                         <div className="budgetItem" style={{fontSize:"15px"}}>NGN {hooks.formatNumber(budget.expenseList[0].amountRequired)}</div>
                         <div className="budgetItem" style={{fontSize:"12px"}}>{budget.expenseList[0].firstName} {budget.expenseList[0].date.slice(0,6)} </div>
                         <div className="budgetItem" style={{fontSize:"12px"}}>{budget.expenseList[0].username}</div>
-                        <div style={{display:"flex",gap:"4px", justifyContent:"space-between"}}>
-                        { user?.email==="mogaleza@gmail.com" && <div onClick={()=> handleApprove(item?._id, budget.expenseList[0]._id, budget.expenseList[0], "approved")} className="approve-btn elevate"
-                        style={{display: budget.expenseList[0].status==="pending"? "block" : "none"}} >{budget.expenseList[0].status? "confirm" : ""}</div>}
-                        { user?.email==="mogaleza@gmail.com" && <div onClick={()=> handleApprove(item?._id, budget.expenseList[0]._id, budget.expenseList[0], "denied")} className="reject-btn elevate" style={{  display: budget.expenseList[0].status==="pending"? "block" : "none"}}>{budget.expenseList[0].status? "reject" : ""}</div>}
-                        { <button style={{display: budget.expenseList[0].status==="pending"? "block": budget.expenseList[0].status==="denied" ? "block" : "none"}} onClick={()=> deleteBudget(item?._id, budget?._id)} className="elevate delete-budget-btn">X</button> }
-                        </div>
                     {/* </section> */}
                     </div>
                 ))}
                 </section>
             </div>
         ))}
+        { fullPost &&
+        <section 
+         style={{
+          background: "rgba(45, 45, 45, 0.6)",
+          width: "100vw",
+          height:"100vh",
+          display:"flex", justifyContent:"center", alignItems:"center",
+          position: "fixed",
+          top: "0", left:"0", right:"", bottom:"0",
+        }}>
+            <div
+            style={{
+                // background:"white",
+                backgroundColor: budgetColor(budgetItem.status),
+
+                color:"white",
+                // width : "fit",
+                padding: "20px",
+                borderRadius: "2px",
+                // textAlign: "center",
+                gap:"4px"
+            }}>
+                <div style={{float:"right", marginLeft:"10px"}}>
+                <button onClick={()=> setFullPost(prev=> !prev)}>X</button>
+                </div>
+                 <div className="budgetItem" style={{fontSize:"14px", textTransform:"uppercase", color:"black", fontWeight:"650"}}>{budgetItem.purpose}</div>
+                 <div className="budgetItem" style={{textAlign:"start",fontSize:"14px", fontStyle:"italic"}}>{ budgetItem.detail}</div>
+                 
+                 <div className="budgetItem" style={{fontSize:"15px", marginTop:"8px"}}>NGN {hooks.formatNumber(budgetItem.amountRequired)}</div>
+                 <div className="budgetItem" style={{fontSize:"12px"}}>{budgetItem.firstName} {budgetItem.lastName} </div>
+                 <div className="budgetItem" style={{fontSize:"12px"}}>{budgetItem.date} </div>
+                 <div className="budgetItem" style={{fontSize:"12px"}}>{budgetItem.username}</div>
+
+                 <div style={{display:"flex",gap:"40px", justifyContent:"space-between", paddingTop:"15px"}}>
+                        { user?.email==="mogaleza@gmail.com" && <div onClick={()=>{setFullPost(prev=> !prev);  handleApprove(postItem?._id, budgetItem._id, budgetItem, "approved")}} className="approve-btn elevate"
+                        style={{display: budgetItem.status==="pending"? "block" : "none"}} >{budgetItem.status? "confirm" : ""}</div>}
+                        { user?.email==="mogaleza@gmail.com" && <div onClick={()=>{setFullPost(prev=> !prev);  handleApprove(postItem?._id, budgetItem?._id, budgetItem, "denied")}} className="reject-btn elevate" style={{  display: budgetItem.status==="pending"? "block" : "none"}}>{budgetItem.status? "reject" : ""}</div>}
+                        { <button style={{display: budgetItem.status==="pending"? "block": budgetItem.status==="denied" ? "block" : "none"}} onClick={()=>{setFullPost(prev=> !prev); deleteBudget(postItem?._id, budgetId)}} className="elevate delete-budget-btn">delete</button> }
+
+                </div>            
+            </div>
+        </section>
+        }
     </div>
   )
 }
